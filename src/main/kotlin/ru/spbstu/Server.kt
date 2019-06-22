ca430e521a8f3bc571a59de5c39c8fbdaf91fcd4
saloed <napster_1997@mail.ru>
2019-06-22 16:05:25 +0300

    add lambda blockchain server

--- src/main/kotlin/ru/spbstu/Server.kt
+++ src/main/kotlin/ru/spbstu/Server.kt
@@ -5,7 +5,6 @@ import com.sun.net.httpserver.HttpServer
 import java.io.File
 import java.net.InetSocketAddress
 import java.net.URL
-import java.nio.file.Path
 
 
 data class SubmitParams(val block_num: Int, val sol_path: String, val desc_path: String)
@@ -19,25 +18,14 @@ object Server {
         client("submit", params)
     }
 
-    fun processBlock(path: String) {
+    fun processBlock(path: String){
         val blockDir = File(path)
         val blockNumber = Integer.valueOf(blockDir.name)
 
         println(path)
         println(blockNumber)
 
-        Main.main(listOf(
-                "--sol-folder", "forLambdaChain/$blockNumber",
-                "--map", Path.of(blockDir.path, "task.desc").toFile().absolutePath,
-                "--threads", "7",
-                "--use-absolute-map-path"
-        ))
 
-        val block_num = blockNumber
-        val sol_path = "forLambdaChain/$blockNumber/task.sol"
-        val desc_path = ""
-
-        submitResults(SubmitParams(block_num, sol_path, desc_path))
     }
 
 
