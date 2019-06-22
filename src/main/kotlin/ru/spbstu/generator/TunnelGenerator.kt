e5c15289fb17332183cee05416b776830e24a6f7
Sergei Vorobyov <Sergey.Vorobyov@jetbrains.com>
2019-06-22 21:22:48 +0300

    asdasd

--- src/main/kotlin/ru/spbstu/generator/TunnelGenerator.kt
+++ src/main/kotlin/ru/spbstu/generator/TunnelGenerator.kt
@@ -1,11 +1,94 @@
 package ru.spbstu.generator
 
+import ru.spbstu.generator.TunnelGenerator.FragmentType.*
 import ru.spbstu.map.*
 import ru.spbstu.player.aStarSearch
 
 
 class TunnelGenerator(private val parameters: Parameters) {
     enum class Cell { WALL, PATH }
+
+    enum class FragmentType {
+        INNER_CORNER_UL, INNER_CORNER_UR, INNER_CORNER_DL, INNER_CORNER_DR,
+        OUTER_CORNER_UL, OUTER_CORNER_UR, OUTER_CORNER_DL, OUTER_CORNER_DR,
+        LONG_CORNER_000000011, LONG_CORNER_000000110, LONG_CORNER_011000000, LONG_CORNER_110000000,
+        LONG_CORNER_001001000, LONG_CORNER_000001001, LONG_CORNER_100100000, LONG_CORNER_000100100,
+        PLACE_U, PLACE_L, PLACE_D, PLACE_R
+    }
+
+    private val CORNERS = setOf(INNER_CORNER_UL, INNER_CORNER_UR, INNER_CORNER_DL, INNER_CORNER_DR,
+            OUTER_CORNER_UL, OUTER_CORNER_UR, OUTER_CORNER_DL, OUTER_CORNER_DR)
+
+    private fun getFragmentTypes(point: Point): Set<FragmentType> {
+        return sequence {
+            if (isWall(point) && !isWall(point.up()) && !isWall(point.left()) && !isWall(point.up().left()))
+                yield(OUTER_CORNER_UL)
+            if (isWall(point) && !isWall(point.up()) && !isWall(point.right()) && !isWall(point.up().right()))
+                yield(OUTER_CORNER_UR)
+            if (isWall(point) && !isWall(point.down()) && !isWall(point.left()) && !isWall(point.down().left()))
+                yield(OUTER_CORNER_DL)
+            if (isWall(point) && !isWall(point.down()) && !isWall(point.right()) && !isWall(point.down().right()))
+                yield(OUTER_CORNER_DR)
+            if (isWall(point) && isWall(point.up()) && isWall(point.left()) && !isWall(point.up().left()))
+                yield(INNER_CORNER_UL)
+            if (isWall(point) && isWall(point.up()) && isWall(point.right()) && !isWall(point.up().right()))
+                yield(INNER_CORNER_UR)
+            if (isWall(point) && isWall(point.down()) && isWall(point.left()) && !isWall(point.down().left()))
+                yield(INNER_CORNER_DL)
+            if (isWall(point) && isWall(point.down()) && isWall(point.right()) && !isWall(point.down().right()))
+                yield(INNER_CORNER_DR)
+            if (isWall(point) && isWall(point.left()) && isWall(point.right()) &&
+                    !isWall(point.up()) && !isWall(point.up().left()) && !isWall(point.up().right()) &&
+                    !isWall(point.up().up()) && !isWall(point.up().up().left()) && !isWall(point.up().up().right()))
+                yield(PLACE_U)
+            if (isWall(point) && isWall(point.left()) && isWall(point.right()) &&
+                    !isWall(point.down()) && !isWall(point.down().left()) && !isWall(point.down().right()) &&
+                    !isWall(point.down().down()) && !isWall(point.down().down().left()) && !isWall(point.down().down().right()))
+                yield(PLACE_D)
+            if (isWall(point) && isWall(point.up()) && isWall(point.down()) &&
+                    !isWall(point.left()) && !isWall(point.left().up()) && !isWall(point.left().down()) &&
+                    !isWall(point.left().left()) && !isWall(point.left().left().up()) && !isWall(point.left().left().down()))
+                yield(PLACE_L)
+            if (isWall(point) && isWall(point.up()) && isWall(point.down()) &&
+                    !isWall(point.right()) && !isWall(point.right().up()) && !isWall(point.right().down()) &&
+                    !isWall(point.right().right()) && !isWall(point.right().right().up()) && !isWall(point.right().right().down()))
+                yield(PLACE_R)
+
+            if (isWall(point) && !isWall(point.left()) && isWall(point.right()) &&
+                    !isWall(point.up()) && !isWall(point.up().left()) && !isWall(point.up().right()) &&
+                    !isWall(point.up().up()) && !isWall(point.up().up().left()) && !isWall(point.up().up().right()))
+                yield(LONG_CORNER_000000011)
+            if (isWall(point) && isWall(point.left()) && !isWall(point.right()) &&
+                    !isWall(point.up()) && !isWall(point.up().left()) && !isWall(point.up().right()) &&
+                    !isWall(point.up().up()) && !isWall(point.up().up().left()) && !isWall(point.up().up().right()))
+                yield(LONG_CORNER_000000110)
+            if (isWall(point) && !isWall(point.left()) && isWall(point.right()) &&
+                    !isWall(point.down()) && !isWall(point.down().left()) && !isWall(point.down().right()) &&
+                    !isWall(point.down().down()) && !isWall(point.down().down().left()) && !isWall(point.down().down().right()))
+                yield(LONG_CORNER_011000000)
+            if (isWall(point) && isWall(point.left()) && !isWall(point.right()) &&
+                    !isWall(point.down()) && !isWall(point.down().left()) && !isWall(point.down().right()) &&
+                    !isWall(point.down().down()) && !isWall(point.down().down().left()) && !isWall(point.down().down().right()))
+                yield(LONG_CORNER_110000000)
+
+            if (isWall(point) && isWall(point.up()) && !isWall(point.down()) &&
+                    !isWall(point.left()) && !isWall(point.left().up()) && !isWall(point.left().down()) &&
+                    !isWall(point.left().left()) && !isWall(point.left().left().up()) && !isWall(point.left().down()))
+                yield(LONG_CORNER_001001000)
+            if (isWall(point) && !isWall(point.up()) && isWall(point.down()) &&
+                    !isWall(point.left()) && !isWall(point.left().up()) && !isWall(point.left().down()) &&
+                    !isWall(point.left().left()) && !isWall(point.left().left().up()) && !isWall(point.left().down()))
+                yield(LONG_CORNER_000001001)
+            if (isWall(point) && isWall(point.up()) && !isWall(point.down()) &&
+                    !isWall(point.right()) && !isWall(point.right().up()) && !isWall(point.right().down()) &&
+                    !isWall(point.right().right()) && !isWall(point.right().right().up()) && !isWall(point.right().down()))
+                yield(LONG_CORNER_100100000)
+            if (isWall(point) && !isWall(point.up()) && isWall(point.down()) &&
+                    !isWall(point.right()) && !isWall(point.right().up()) && !isWall(point.right().down()) &&
+                    !isWall(point.right().right()) && !isWall(point.right().right().up()) && !isWall(point.right().down()))
+                yield(LONG_CORNER_000100100)
+        }.toSet()
+    }
 
     private val matrix = HashMap<Point, Cell>()
 
@@ -34,48 +117,108 @@ class TunnelGenerator(private val parameters: Parameters) {
         points.forEach { matrix[it] = Cell.WALL }
     }
 
-    private fun appendCorners(corners: Int) {
-        println(corners)
+    private fun addWallIfCan(point: Point): Boolean {
+        if (point.v0 < 0 || point.v1 < 0) return false
+        if (point.v0 >= parameters.mapSize) return false
+        if (point.v1 >= parameters.mapSize) return false
+        if (isWall(point)) return false
+        if (falseWall(point)) return false
+        matrix[point] = Cell.WALL
+        return true
     }
 
-    private fun removeCorners(corners: Int) {
+    private fun appendCorners() {
+        var availableCorners = parameters.verticesMin - countCorners()
+        var pointsIt = emptyList<Point>().iterator()
+        var previousAvailableCorners = availableCorners + 1
+        while (availableCorners > 0) {
+            if (!pointsIt.hasNext()) {
+                if (previousAvailableCorners == availableCorners) {
+                    println("Has not any possibilities to append, $availableCorners")
+                    return
+                }
+                previousAvailableCorners = availableCorners
+                pointsIt = matrix.filter { it.value == Cell.WALL }.map { it.key }.iterator()
+            }
+            val point = pointsIt.next()
+            val fragmentsType = getFragmentTypes(point)
+            if (PLACE_U in fragmentsType) {
+                if (addWallIfCan(point.up())) {
+                    availableCorners -= 4
+                }
+            } else if (PLACE_D in fragmentsType) {
+                if (addWallIfCan(point.down())) {
+                    availableCorners -= 4
+                }
+            } else if (PLACE_L in fragmentsType) {
+                if (addWallIfCan(point.left())) {
+                    availableCorners -= 4
+                }
+            } else if (PLACE_R in fragmentsType) {
+                if (addWallIfCan(point.right())) {
+                    availableCorners -= 4
+                }
+            } else if (LONG_CORNER_000000011 in fragmentsType) {
+                if (addWallIfCan(point.up())) {
+                    availableCorners -= 2
+                }
+            } else if (LONG_CORNER_000000110 in fragmentsType) {
+                if (addWallIfCan(point.up())) {
+                    availableCorners -= 2
+                }
+            } else if (LONG_CORNER_110000000 in fragmentsType) {
+                if (addWallIfCan(point.down())) {
+                    availableCorners -= 2
+                }
+            } else if (LONG_CORNER_011000000 in fragmentsType) {
+                if (addWallIfCan(point.down())) {
+                    availableCorners -= 2
+                }
+            } else if (LONG_CORNER_001001000 in fragmentsType) {
+                if (addWallIfCan(point.left())) {
+                    availableCorners -= 2
+                }
+            } else if (LONG_CORNER_000001001 in fragmentsType) {
+                if (addWallIfCan(point.left())) {
+                    availableCorners -= 2
+                }
+            } else if (LONG_CORNER_100100000 in fragmentsType) {
+                if (addWallIfCan(point.right())) {
+                    availableCorners -= 2
+                }
+            } else if (LONG_CORNER_000100100 in fragmentsType) {
+                if (addWallIfCan(point.right())) {
+                    availableCorners -= 2
+                }
+            }
+        }
+    }
+
+    private fun removeCorners() {
 
     }
 
     fun generate(): HashMap<Point, Cell> {
         parameters.pathsPoints.forEach { matrix[it] = Cell.PATH }
         for (i in -1..parameters.mapSize) {
-            matrix[Point(i, 0)] = Cell.WALL
-            matrix[Point(0, i)] = Cell.WALL
+            matrix[Point(i, -1)] = Cell.WALL
+            matrix[Point(-1, i)] = Cell.WALL
             matrix[Point(i, parameters.mapSize)] = Cell.WALL
             matrix[Point(parameters.mapSize, i)] = Cell.WALL
         }
         parameters.wallsPoints.forEach {
             generateWalls(it)
         }
-        val corners = countCorners()
-        if (corners > parameters.verticesMax) {
-            removeCorners(corners - parameters.verticesMax)
-        } else if (corners < parameters.verticesMin) {
-            appendCorners(parameters.verticesMin - corners)
-        }
-        println(parameters.verticesMin to parameters.verticesMax to countCorners())
+        appendCorners()
+        removeCorners()
+        println("${countCorners()} in ${parameters.verticesMin to parameters.verticesMax}")
         return matrix
     }
 
-    private fun countCorners() = matrix.filter { it.value == Cell.WALL }.map { it.key }.count { isCorner(it) }
-
-    private fun isOuterCorner(point: Point) = isWall(point) &&
-            (!isWall(point.up()) && !isWall(point.left()) && !isWall(point.up().left()) ||
-                    !isWall(point.up()) && !isWall(point.right()) && !isWall(point.up().right()) ||
-                    !isWall(point.down()) && !isWall(point.left()) && !isWall(point.down().left()) ||
-                    !isWall(point.down()) && !isWall(point.right()) && !isWall(point.down().right()))
-
-    private fun isInnerCorner(point: Point) = isWall(point) &&
-            (isWall(point.up()) && isWall(point.left()) && !isWall(point.up().left()) ||
-                    isWall(point.up()) && isWall(point.right()) && !isWall(point.up().right()) ||
-                    isWall(point.down()) && isWall(point.left()) && !isWall(point.down().left()) ||
-                    isWall(point.down()) && isWall(point.right()) && !isWall(point.down().right()))
-
-    private fun isCorner(point: Point) = isOuterCorner(point) || isInnerCorner(point)
+    private fun countCorners() = matrix.asSequence()
+            .filter { it.value == Cell.WALL }
+            .map { it.key }
+            .map { getFragmentTypes(it) }
+            .map { it.count { type -> type in CORNERS } }
+            .sum()
 }
